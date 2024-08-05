076310501ec64d153233dbe7a0e46360f07fd137
Lyndonn Pamintuan <pamintuan.lyndonn0503@gmail.com>
2024-08-06 07:27:33 +0800

    init setup

---
+++ src/main/java/io/lpamintuan/spring_sec_ap/SpringSecApApplication.java
@@ -0,0 +1,13 @@
+package io.lpamintuan.spring_sec_ap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecApApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecApApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/lpamintuan/spring_sec_ap/SpringSecApApplicationTests.java
@@ -0,0 +1,13 @@
+package io.lpamintuan.spring_sec_ap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecApApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
